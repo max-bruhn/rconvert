d7b979e2b314c8faa1a43494bff993cb5ba13e83
Max Bruhn <mpcbruhn+git@gmail.com>
2020-06-13 15:41:30 +0700

    basic input styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useRef } from 'react'
 import StateContext from '../../StateContext'
 import DispatchContext from '../../DispatchContext'
 import { useImmer } from 'use-immer'
@@ -9,7 +9,26 @@ import { Draggable } from 'react-beautiful-dnd'
 import styles from './Card.module.scss'
 import transition from './transition.module.scss'
 
+function useTraceUpdate(props) {
+  const prev = useRef(props)
+  useEffect(() => {
+    const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
+      if (prev.current[k] !== v) {
+        ps[k] = [prev.current[k], v]
+      }
+      return ps
+    }, {})
+    if (Object.keys(changedProps).length > 0) {
+      console.log('Changed props:', changedProps)
+    }
+    prev.current = props
+  })
+}
+
 const Card = (props) => {
+  useTraceUpdate(props)
+  console.log(props + ' ' + Date.now())
+
   const appState = useContext(StateContext)
   const appDispatch = useContext(DispatchContext)
 
@@ -55,6 +74,14 @@ const Card = (props) => {
   // ref for css transition (otherwise throws warning in strict mode)
   const nodeRef = React.createRef()
 
+  function Input() {
+    return <input type="number" className="float-right bg-opacity-25 bg-white rounded-lg text-right align-bottom  font-bold text-xl " />
+  }
+
+  function Amount() {
+    return <span className="float-right align-bottom leading-10 font-bold text-xl ">300</span>
+  }
+
   return (
     // set id to props.currency as string, so it can easily parsed as obj when drag ends (inside content.jsx onDragEnd handler)
     <Draggable draggableId={JSON.stringify(props.currency)} index={props.id}>
@@ -65,7 +92,7 @@ const Card = (props) => {
               <div className="font-bold text-xl pb-2">{props.currency.value}</div>
               <div className="w-full">
                 <span className="float-left align-bottom leading-10">{props.currency.label}</span>
-                <span className="float-right align-bottom leading-10 font-bold text-xl ">300</span>
+                {props.id == 0 ? <Input /> : <Amount />}
               </div>
             </div>
           </CSSTransition>
