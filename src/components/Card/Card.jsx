660bdb7a1177fff9b5dcd39f33bd5a5b43d88ea6
Max Bruhn <mpcbruhn+git@gmail.com>
2020-06-13 15:18:40 +0700

    basic amount styling added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -63,7 +63,10 @@ const Card = (props) => {
           <CSSTransition nodeRef={nodeRef} in={state.display} timeout={2000} classNames={transition} unmountOnExit>
             <div ref={nodeRef} className={`h-24  rounded-lg p-4 text-gray-200 ${styles.gradient} `}>
               <div className="font-bold text-xl pb-2">{props.currency.value}</div>
-              <div>{props.currency.label}</div>
+              <div className="w-full">
+                <span className="float-left align-bottom leading-10">{props.currency.label}</span>
+                <span className="float-right align-bottom leading-10 font-bold text-xl ">300</span>
+              </div>
             </div>
           </CSSTransition>
         </div>
