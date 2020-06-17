48d0e9d263e244183ecfd41b08a3dae51ce4cca9
Max Bruhn <mpcbruhn+git@gmail.com>
2020-06-17 12:57:47 +0700

    delete card icon now svg. added tooltip

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -145,9 +145,11 @@ const Card = (props) => {
             <div ref={nodeRef} className={`h-24 overflow-hidden rounded-lg p-4 text-gray-200 ${styles.gradient} `}>
               <div className="font-bold text-xl pb-2 w-2/4 inline">{props.currency.value}</div>
               <div className="inline float-right ">
-                <div onClick={deleteHandler} className={styles.wrapper}>
-                  <div className={`${styles.dash} ${styles.left}`}></div>
-                  <div className={`${styles.dash} ${styles.right}`}></div>
+                <div onClick={deleteHandler} className={`w-3 h-3 cursor-pointer ${styles.tooltip}`}>
+                  <svg className={`w-3 h-3 fill-current `} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496.096 496.096">
+                    <path d="M259.41 247.998L493.754 13.654a8 8 0 000-11.312 8 8 0 00-11.312 0L248.098 236.686 13.754 2.342A8 8 0 002.442 13.654l234.344 234.344L2.442 482.342a8 8 0 00-.196 11.312 8 8 0 0011.508 0L248.098 259.31l234.344 234.344a8 8 0 0011.312-.196 8 8 0 000-11.116L259.41 247.998z" />
+                  </svg>
+                  <span className={`-mt-12 -ml-20 bg-grey-600 p-1 rounded ${styles['tooltip-text']}`}>Remove Me!</span>
                 </div>
               </div>
               <div className="w-full">
