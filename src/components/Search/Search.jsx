83e4bc9bcee677fa2ef0d4013ba2a43710757f8b
Max Bruhn <mpcbruhn+git@gmail.com>
2020-06-17 11:55:10 +0700

    code cleanup

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -176,7 +176,7 @@ const Search = () => {
   return (
     <>
       <div ref={searchDiv} className={`${styles.input} w-full bg-gray-900 text-gray-600`}>
-        <div className="px-3 flex border rounded-lg border-gray-600  text-gray-600 w-full bg-gray-900">
+        <div className="px-3 flex border rounded-lg border-gray-600 text-gray-600 w-full bg-gray-900">
           <svg className="fill-current w-4 hj-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
           </svg>
